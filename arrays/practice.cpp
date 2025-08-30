41285d9a856976e707d87d88f7732b633875aebb
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2025-08-30 23:32:17 +0530

    Update practice.cpp

--- arrays/practice.cpp
+++ arrays/practice.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main () {
-	string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
-
-	for (int car = 0; car < sizeof(cars)/sizeof(cars[0]); car++) {
-		cout << cars[car] << " ";
-	}
-
-	cout << "again this was a necessary commit!!!, have to commit again" << endl;
-	return 0;
-}
